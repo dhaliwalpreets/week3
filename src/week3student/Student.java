ca8fdf4874f9a75a4b4778d5a65919dc2cbe65d9
Preet singh dhaliwal <dhpreets@sheridancollege.ca>
2019-05-22 09:35:08 -0400

    commit....

---
+++ src/week3student/Student.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3student;
+
+/**
+ *
+ * @author dhaliwal
+ */
+public class Student {
+    private int id;
+    private String firstname;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the firstname
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * @param firstname the firstname to set
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    
+    
+}
